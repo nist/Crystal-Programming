43a0b2d31d41973a6ae55e6c634a51753c140a0d
Nicolas St-Laurent <stlaurent.nicolas@gmail.com>
2022-10-27 16:26:04 -0400

    Add spec for yaml module.

--- Chapter04/spec/yaml_spec.cr
+++ Chapter04/spec/yaml_spec.cr
@@ -1,13 +1,29 @@
 require "spec"
 require "../src/yaml"
 
-describe "Transform::YAML.deserialize" do
-  pending "should deserialize valid yaml" do
+INPUT_YAML = <<-YAML
+---
+- id: 1
+  author:
+    name: Jim
+- id: 2
+  author:
+    name: Bob
 
+YAML
+
+OUTPUT_JSON = <<-JSON
+[{"id":1,\"author\":{"name":"Jim"}},{"id":2,\"author\":{"name":"Bob"}}]
+JSON
+
+describe "Transform::YAML.deserialize" do
+  it "should deserialize valid yaml to json" do
+      Transform::YAML.deserialize(INPUT_YAML).should eq OUTPUT_JSON
   end
 end
 
 describe "Transform::YAML.serialize" do
-  pending "should serialize valid yaml" do
+  it "should serialize valid yaml" do
+    Transform::YAML.serialize(OUTPUT_JSON).should eq INPUT_YAML
   end
 end
